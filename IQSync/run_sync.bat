28b1fe62c2f747e711e8efee90786941b621163d
Ayaan Gangat <105651025+AyaanGangat@users.noreply.github.com>
2025-05-19 08:39:26 +0200

    Update run_sync.bat

--- IQSync/run_sync.bat
+++ IQSync/run_sync.bat
@@ -1,9 +1,6 @@
 @echo off
-echo Starting PHP script with explicit php.ini... >> C:\IQSync\sync_activity_log.txt
-echo %date% %time% >> C:\IQSync\sync_activity_log.txt
-C:\PHP\php.exe -c C:\PHP\php.ini C:\IQSync\sync_stock.php >> C:\IQSync\sync_activity_log.txt 2>&1
-echo Script execution finished. >> C:\IQSync\sync_activity_log.txt
-echo -------------------------------------------------- >> C:\IQSync\sync_activity_log.txt
-REM You can remove the 'pause' if this is only for scheduled tasks,
-REM or keep it if you also run the .bat manually for testing.
+echo Starting PHP script with explicit php.ini...
+C:\PHP\php.exe -c C:\PHP\php.ini C:\IQSync\sync_stock.php
+echo.
+echo Script execution finished.
 pause
